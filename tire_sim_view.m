225f3201525c75d5fdbcc743a0bb4f1637cbd936
kgabbott <kvnabbott@gmail.com>
2015-10-25 16:02:24 -0400

    Added slip/force graph

--- tire_sim_view.m
+++ tire_sim_view.m
@@ -1,5 +1,5 @@
 function tire_sim_view
-global x1 x2 theta1 theta2;
+global x1 x2 theta1 theta2 slip1 slip2;
 % radius (m)
 r = .4;
 
@@ -11,8 +11,36 @@ while(x1(low_lim)<.001)
     low_lim = low_lim+1;
 end
 
+
+clf;
+wheel_plot = subplot(211);
+
+slip_force = subplot(212);
+grid on;
+s = linspace(-1,1,1000);
+plot(s, sin(2*atan((5*s)-1*((3*s)-atan(3*s)))));
+hold on;
+wheelrear_sf = plot(0,0);
+wheelfront_sf = plot(0,0);
+
 for t = low_lim:length(x1)
-    clf;
+    
+    subplot(slip_force);
+    grid on;
+    delete(wheelrear_sf);
+    delete(wheelfront_sf);
+    
+    slip_1 = slip1(t);
+    force_1 = sin(2*atan((5*slip1(t))-1*((3*slip1(t))-atan(3*slip1(t)))));
+    wheelrear_sf = text(slip_1, force_1, '\leftarrow Rear');
+    
+    slip_2 = slip2(t);
+    force_2 = sin(2*atan((5*slip2(t))-1*((3*slip2(t))-atan(3*slip2(t)))));
+    wheelfront_sf = text(slip_2, force_2, '\leftarrow Front');  
+    
+    
+    cla(wheel_plot);
+    subplot(wheel_plot);
     line(x_lim, [0 0]);
     hold on;
     
@@ -28,9 +56,9 @@ for t = low_lim:length(x1)
     set(gca, 'YTickLabel', []);
     set(gca, 'YTick', []);
     % Pause for dramatic effect
-    pause(.1);    
+    pause(.1);
+    hold off;
 end
-hold off;
 end
 
 function [p, p2]=circleplot(x,y,r, theta)

d91c797d0b8771141743ff27453f934764b0746e
kgabbott <kvnabbott@gmail.com>
2015-12-06 17:25:58 -0500

    added brakes

--- tire_sim_view.m
+++ tire_sim_view.m
@@ -1,5 +1,5 @@
 function tire_sim_view
-global x1 x2 theta1 theta2 slip1 slip2;
+global x_rear x_front theta_rear theta_front slip_rear slip_front;
 % radius (m)
 r = .4;
 
@@ -7,7 +7,7 @@ x_lim = [-2 30];
 y_lim = [-1 4];
 
 low_lim = 1;
-while(x1(low_lim)<.001)
+while(x_rear(low_lim)<.001)
     low_lim = low_lim+1;
 end
 
@@ -16,6 +16,9 @@ clf;
 wheel_plot = subplot(211);
 
 slip_force = subplot(212);
+title('Slip/Force Graph');
+xlabel('Wheel Slip');
+ylabel('Tractive Force');
 grid on;
 s = linspace(-1,1,1000);
 plot(s, sin(2*atan((5*s)-1*((3*s)-atan(3*s)))));
@@ -23,19 +26,22 @@ hold on;
 wheelrear_sf = plot(0,0);
 wheelfront_sf = plot(0,0);
 
-for t = low_lim:length(x1)
+for t = low_lim:length(x_rear)
     
     subplot(slip_force);
+    title('Slip/Force Graph');
+    xlabel('Wheel Slip');
+    ylabel('Tractive Force');
     grid on;
     delete(wheelrear_sf);
     delete(wheelfront_sf);
     
-    slip_1 = slip1(t);
-    force_1 = sin(2*atan((5*slip1(t))-1*((3*slip1(t))-atan(3*slip1(t)))));
+    slip_1 = slip_rear(t);
+    force_1 = sin(2*atan((5*slip_rear(t))-1*((3*slip_rear(t))-atan(3*slip_rear(t)))));
     wheelrear_sf = text(slip_1, force_1, '\leftarrow Rear');
     
-    slip_2 = slip2(t);
-    force_2 = sin(2*atan((5*slip2(t))-1*((3*slip2(t))-atan(3*slip2(t)))));
+    slip_2 = slip_front(t);
+    force_2 = sin(2*atan((5*slip_front(t))-1*((3*slip_front(t))-atan(3*slip_front(t)))));
     wheelfront_sf = text(slip_2, force_2, '\leftarrow Front');  
     
     
@@ -45,9 +51,9 @@ for t = low_lim:length(x1)
     hold on;
     
     % Visualize output
-    circleplot(x1(t), r, r, -1*theta1(t));
-    circleplot(x2(t), r, r, -1*theta2(t));
-    frame = line([x1(t) x2(t)], [r r]);
+    circleplot(x_rear(t), r, r, -1*theta_rear(t));
+    circleplot(x_front(t), r, r, -1*theta_front(t));
+    frame = line([x_rear(t) x_front(t)], [r r]);
     set(frame, 'Color', 'black');
     
     axis equal
